6f66c7665055e012045d6a171a345e99d1df4eb9
Niels Doucet <niels.doucet@collibra.com>
2019-06-13 15:23:10 +0200

    add javadoc to MathUtils

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -2,6 +2,14 @@ package com.example.shipkitworkshop;
 
 public class MathUtils {
 
+    /**
+     * Checks if these coordinates form a triangle.
+     *
+     * @param a first point
+     * @param b second point
+     * @param c third point
+     * @return {@code true} if this is triangle
+     */
     public static boolean isTriangle(int a, int b, int c) {
         return a + b > c
                 && a + c > b
